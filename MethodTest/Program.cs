c8cab5921288c332fb24ce9efc24b91d4c664739
Lanna <Lanna_lmb@hotmail.com>
2021-07-30 23:56:46 -0400

    removed unused input variable, added access modifiers to class and main method, cleaned up spacing, added Thread.Sleep(3000) to see results, removed unused usings.

--- MethodTest/Program.cs
+++ MethodTest/Program.cs
@@ -1,46 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace MethodTest
 {
-    class Program
+    public class Program
     {
-        private static int input;
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
             // Declare Variables
             int x, y;
-
 
             // Call Method
             x = GetValidInteger();
             y = GetValidInteger();
 
-
             // Process
             int result = x * y;
             Console.WriteLine(result);
 
-
             // Call PrintNumbers Method
-            PrintNumbers(1000);
-
+            PrintNumbers(3);
         }
 
-
-
-        //Method to Return Integer
+        // Method to Return Integer
         public static int GetValidInteger()
         {
             int input;
 
             Console.WriteLine("Please Enter an Integer: ");
-
 
             // Validation
             while (!int.TryParse(Console.ReadLine(), out input))
@@ -51,14 +38,13 @@ namespace MethodTest
             return input;  // return input to the caller method
         }
 
-
         public static void PrintNumbers(int number)
         {
             for (int i = 1; i <= number; i++)
             {
                 Console.WriteLine(i);
-
+                Thread.Sleep(3000);
             }
         }
-    }        
+    }
 }
